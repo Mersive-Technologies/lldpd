97442e88d3e7cd4e93b1f7f3052d7bce48471ea6
Adam Schmackpfeffer <aschmackpfeffer@mersive.com>
2021-04-28 14:25:01 -0600

    Add a version report to copy into image repo

--- mersive/build.sh
+++ mersive/build.sh
@@ -12,3 +12,11 @@ docker run \
     -v ${LLDPD_DIR}:/home/devel/lldpd \
     -- \
     lldpd_build
+
+echo "LLDPD built from $(git remote -v | grep fetch | cut -f2 | cut -f1 -d' ')" > ${LLDPD_DIR}/dist/VERSION
+echo "  built on $(hostname) by $(git config --get user.name) [$(git config --get user.email)] at $(date)" >> ${LLDPD_DIR}/dist/VERSION
+echo "  current branch is $(git branch --show-current)" >> ${LLDPD_DIR}/dist/VERSION
+echo "  current commit is $(git rev-parse HEAD)" >> ${LLDPD_DIR}/dist/VERSION
+echo "" >> ${LLDPD_DIR}/dist/VERSION
+echo "  git status is:" >> ${LLDPD_DIR}/dist/VERSION
+git status | grep -v libevent | sed "s/^/    /g" >> ${LLDPD_DIR}/dist/VERSION
