57a93e7af1ee4049ad7fc13ee8c9bdee01f9f90f
Adam Schmackpfeffer <aschmackpfeffer@mersive.com>
2021-04-28 14:28:37 -0600

    Clean up unwanted build artifacts

--- mersive/build.sh
+++ mersive/build.sh
@@ -13,10 +13,13 @@ docker run \
     -- \
     lldpd_build
 
+# Doing a build dirties this directory. We don't want to leave repo dirty.
+rm -rf ${LLDPD_DIR}/libevent/build-aux
+
 echo "LLDPD built from $(git remote -v | grep fetch | cut -f2 | cut -f1 -d' ')" > ${LLDPD_DIR}/dist/VERSION
 echo "  built on $(hostname) by $(git config --get user.name) [$(git config --get user.email)] at $(date)" >> ${LLDPD_DIR}/dist/VERSION
 echo "  current branch is $(git branch --show-current)" >> ${LLDPD_DIR}/dist/VERSION
 echo "  current commit is $(git rev-parse HEAD)" >> ${LLDPD_DIR}/dist/VERSION
 echo "" >> ${LLDPD_DIR}/dist/VERSION
 echo "  git status is:" >> ${LLDPD_DIR}/dist/VERSION
-git status | grep -v libevent | sed "s/^/    /g" >> ${LLDPD_DIR}/dist/VERSION
+git status | sed "s/^/    /g" >> ${LLDPD_DIR}/dist/VERSION
